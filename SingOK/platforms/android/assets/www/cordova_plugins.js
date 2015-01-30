09f9bc6e021f6b1a925ddefbf6ee835c52179ae9
dnduc456 <dnduc456@gmail.com>
2015-01-30 01:10:34 -0800

    init

---
+++ SingOK/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.phonegap.plugins.facebookconnect/facebookConnectPlugin.js",
+        "id": "com.phonegap.plugins.facebookconnect.FacebookConnectPlugin",
+        "clobbers": [
+            "facebookConnectPlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.phonegap.plugins.facebookconnect": "0.11.0"
+}
+// BOTTOM OF METADATA
+});
